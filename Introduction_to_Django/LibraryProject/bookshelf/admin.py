63ec13e8613f8d51a744be57113ed12f8872221b
emmanuelbilla <immanuelbilla33@gmail.com>
2025-11-02 21:51:59 +0000

    Registered Book model with Django Admin.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book # Import the Book model from .models
+admin.site.register(Book) # Register the Book model with the admin site
