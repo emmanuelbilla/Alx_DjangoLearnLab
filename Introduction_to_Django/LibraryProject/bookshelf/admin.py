4e35b03bfa2b2a9d840bb9e9234d6ffef61785e1
emmanuelbilla <immanuelbilla33@gmail.com>
2025-11-02 21:59:42 +0000

    Modified registration script.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book # Import the Book model from .models
-admin.site.register(Book) # Register the Book model with the admin site
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+   
+admin.site.register(Book, BookAdmin)
