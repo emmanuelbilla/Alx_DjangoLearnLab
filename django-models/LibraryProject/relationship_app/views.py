d190501eefa3549f080e2fc161a24d42a3696f87
emmanuelbilla <immanuelbilla33@gmail.com>
2025-11-17 15:23:32 +0000

    Refactor import statement for DetailView in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Importing the necessary model
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 
 '''
