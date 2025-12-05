a7abe1c64bb88bd8ff59377650b065c9c7ea84c3
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-05 20:44:53 +0000

    Implement BookList view to retrieve and serialize Book objects

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+'''Creating BookList view that extends the rest_framework.generics.ListAPIView.
+Use the BookSerializer to serialize the data and the Book model as the queryset.
+'''
+#Importing necessary modules and classes
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+#Defining the BookList view
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  #Queryset to retrieve all Book objects
+    serializer_class = BookSerializer  #Serializer class to convert Book objects to JSON format
