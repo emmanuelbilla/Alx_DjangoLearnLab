cbd093839b1be935d63dd1dd3b62f25061f86335
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-06 03:09:59 +0000

    Add URL routing for BookViewSet and update urlpatterns

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,9 +5,17 @@ URL configuration for the project."""
 Including a URL pattern that routes to BookList view.'''
 # Import necessary modules
 from django.urls import path
-from .views import BookList
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+#Define a router and register the BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+
 
 # Define URL patterns
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)), #Including the router URLs
 ]
