0d862dd1899de64da11f69659635cdc0f7934ab2
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-07 22:24:25 +0000

    Add user registration and profile views with corresponding URL patterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,13 +4,20 @@ Urls for the blog app.
 # Import necessary modules
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 #Define URL patterns
 urlpatterns = [
+    # Registration URL
+    path('register/', views.register_view, name='register'),
+
     # Login URL
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
 
     # Logout URL
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
+    # Profile URL
+    path('profile/', views.profile_view, name='profile'),
     
 ]
