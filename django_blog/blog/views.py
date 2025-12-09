8bb4d60a2326b459ed95e50417cecfa53933f1e4
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-09 06:26:46 +0000

    Add comment functionality with views for adding, updating, and deleting comments

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -14,8 +14,9 @@ from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
 
 
 def register_view(request):
@@ -102,3 +103,54 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+# View to add a comment to a post as a fbv
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            messages.success(request, "Comment added successfully.")
+            return redirect('post_detail', pk=post.id)
+    else:
+        form = CommentForm()
+    return redirect('post_detail', pk=post.id)
+
+# Update view for existing comment. Only the author can update.
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_edit.html'
+
+    # Ensure the comment author is the logged-in user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    # Test function to ensure only the comment author can edit
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+    
+    # Define success URL after editing comment
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+# Delete view for a comment. Only the author can delete.
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_delete.html'
+
+    # Test function to ensure only the comment author can delete
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+    
+    # Define success URL after deleting comment
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
