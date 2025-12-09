aaeee88ecfd75df5437f838ba8805ceabe5d2254
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-09 05:57:00 +0000

    Add Comment model to support user comments on posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,6 +8,10 @@ Creating a model Post with the fields title, content, published_date, and author
 #Importing User model for author field
 from django.contrib.auth.models import User
 
+# Imports for comment model
+from django.utils import timezone
+from .models import Post
+
 #Defining Post model
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,3 +26,14 @@ class Post(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('post_detail', kwargs={'pk': self.pk})
+    
+#Defining Comment model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
