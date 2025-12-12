a25bdc858b560f73942571afecb2206a8141612a
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-12 05:02:24 +0000

    Add CommentCreateView for creating new comments on posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -154,3 +154,18 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     # Define success URL after deleting comment
     def get_success_url(self):
         return self.object.post.get_absolute_url()
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'  
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
