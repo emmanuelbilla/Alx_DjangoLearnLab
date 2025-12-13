1fa94b37db282da891f17e110690f2e636e13340
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-13 03:43:20 +0000

    Adding tagging functionality to post and migrating changes

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,12 +12,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 #from .models import Post
 
+# Importing model for tag functionality
+from taggit.managers import TaggableManager 
+
+
 #Defining Post model
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = TaggableManager(blank=True)  # Adding tagging functionality
 
     def __str__(self):
         return self.title
