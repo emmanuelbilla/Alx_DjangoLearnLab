6927b573e67292f343a9adc613dd8eaa6a332c87
emmanuelbilla <immanuelbilla33@gmail.com>
2025-12-13 16:02:14 +0000

    Fix registration serializer bugs and validated_data access

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,24 +1,25 @@
 from rest_framework import serializers
-from .models import User
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+
+
 
 # Serializer for User registration
-class RegisterSelializer(serializers.ModelSerializer):
-    password = serializers.CharField()
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        model = get_user_model()
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password'],
             )
+        
         user.bio = validated_data.get('bio', '')
         user.profile_picture = validated_data.get('profile_picture', None)
         user.save()
@@ -26,4 +27,3 @@ class RegisterSelializer(serializers.ModelSerializer):
         Token.objects.create(user=user)
 
         return user
-    
